ceed9b3d6710b88539f9e332267264ec5bf04ed8
Garrison28 <ghighsmith28@gmail.com>
2019-12-17 14:29:37 -0800

    added link to addphoto

--- client/src/components/AddPhoto.jsx
+++ client/src/components/AddPhoto.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { CloudinaryContext, Transformation, Image } from 'cloudinary-react';
 
 class AddPhoto extends React.Component {
